d9ba115223fd83a4a7b4c6b3bf78aedf0ff0b402
Itsoumyaa <150330646+Itsoumyaa@users.noreply.github.com>
2024-04-14 16:25:49 +0530

    Update ReverseRightHalfpyramid.java

--- ReverseRightHalfpyramid.java
+++ ReverseRightHalfpyramid.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Pattern1 {
+public class ReverseRightHalfpyramid {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
         System.out.println("enter n:  ");
