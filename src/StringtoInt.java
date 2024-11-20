10f0da47e30ae38d44e52231ded20e550334510f
Itsoumyaa <soumya11504@gmail.com>
2024-11-20 09:00:28 +0530

    Added some exceptions to handle errors for invalid inputs.

--- src/StringtoInt.java
+++ src/StringtoInt.java
@@ -2,25 +2,43 @@ import java.util.*;
 public class StringtoInt {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter a string: ");
-        String s=sc.nextLine();
-        int i=0;
-        int sign=1;
-        int res=0;
-        while(i<s.length() && s.charAt(i)==' '){
-            i++;
-        }
-        if(i<s.length() && ((s.charAt(i)=='-') || (s.charAt(i)=='+'))){
-            if(s.charAt(i)=='-'){
-                sign=-1;
-            } i++;
-        }
+        try {
+            System.out.println("enter a string: ");
+            String s = sc.nextLine();
+            int i = 0;
+            int sign = 1;
+            int res = 0;
+            while (i < s.length() && s.charAt(i) == ' ') {
+                i++;
+            }
+            if (i < s.length() && ((s.charAt(i) == '-') || (s.charAt(i) == '+'))) {
+                if (s.charAt(i) == '-') {
+                    sign = -1;
+                }
+                i++;
+            }
+            if(i>=s.length() || !Character.isDigit(s.charAt(i))){
+                throw new IllegalArgumentException("Invalid input: String does not start with a valid number.");
+            }
 
-        while(i<s.length() && Character.isDigit(s.charAt(i))){
-            int digit=s.charAt(i)-'0';
-            res=res*10+digit;
-            i++;
+            while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                int digit = s.charAt(i) - '0';
+                if(res>(Integer.MAX_VALUE-digit)/10){
+                    throw new ArithmeticException("Integer Overflow");
+                }
+                res = res * 10 + digit;
+                i++;
+            } if(i < s.length() && !Character.isDigit(s.charAt(i))){
+                throw new IllegalArgumentException("Invalid input: Contains non-numeric characters.");
+            }
+
+            System.out.println("Converted integer: "+(res*sign));
+        } catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }catch(ArithmeticException e){
+            System.out.println(e.getMessage());
+        } catch(Exception e){
+            System.out.println("An unexpected error occurred");
         }
-        System.out.println(res);
     }
 }
