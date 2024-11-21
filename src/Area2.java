309bd37984dbfef835592df90fdf2964a287913f
Itsoumyaa <soumya11504@gmail.com>
2024-11-21 21:05:28 +0530

    implemented area of circle and rectangle using Math.PI and Math.pow() function.

--- src/Area2.java
+++ src/Area2.java
@@ -1,8 +1,7 @@
 import java.util.*;
-public class Area {
-    static float circle(float r){
-        float pi=3.14f;
-        float area=pi*r*r;
+public class Area2 {
+    static double circle(double r){
+        double area= Math.PI* Math.pow(r,2);
         return area;
     }
     static float rectangle(float l,float b){
@@ -12,7 +11,7 @@ public class Area {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter radius: ");
-        float r= sc.nextFloat();
+        double r= sc.nextDouble();
         System.out.println("area of circle: "+ circle(r));
         System.out.println("enter length and breadth: ");
         float l= sc.nextFloat();
