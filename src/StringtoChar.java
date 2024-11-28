b14ce7b2d917672c7dead9afc1d81077ff59f0d2
Itsoumyaa <soumya11504@gmail.com>
2024-11-28 19:23:14 +0530

    implemented conversion of string to char

---
+++ src/StringtoChar.java
@@ -0,0 +1,10 @@
+public class StringtoChar {
+    public static void main(String[] args) {
+        char c='A';
+        String str= Character.toString(c);
+        System.out.println(str);
+        String s="soumya";
+        char c1= s.charAt(0);
+        System.out.println(c1);
+    }
+}
